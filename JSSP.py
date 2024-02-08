b9f6660643c95ab8b73c16e07a9ece353a1a7adf
Hamad Abdul Razzaq <hr06899@st.habib.edu.pk>
2024-02-08 18:18:24 +0500

    JSSP Initialization done

--- JSSP.py
+++ JSSP.py
@@ -7,26 +7,53 @@ with open("abz5.txt", "r") as file:
 
     for i in range(1,len(lst)):
         Jobs[i-1] = list()
-        current_time = 0
         for j in range(0,len(lst[i]),2):
-            Jobs[i-1].append((lst[i][j], current_time, current_time +  lst[i][j+1]))
-            current_time +=  lst[i][j+1]
+            Jobs[i-1].append((lst[i][j], lst[i][j+1]))
 
         
-print(Jobs)
-current_operation = [0 for _ in range(J)]
+# print(Jobs)
+current_operation = [0 for _ in range(J)]  # [0 0 0 0 0 0 ..... 0]
+'''
+Value = Current Operation Number
+Index = Job No.
+'''
 current_time = [0 for _ in range(M)]
-MOT = dict()
+'''
+Value = Elapsed Time on Machine
+Index = Machine no.
+'''
+prev_operation_end_time = [0 for _ in range(J)]
+
+MOT = dict() # Key: Machine number
 for i in range(M):
     MOT[i] = list()
+# print("MOT", MOT)
+finished_jobs = 0
+iteration = 0
+while finished_jobs != J:
+    current_job = random.randint(0, J-1) # Picking a random job
+    while current_operation[current_job] == M: # Check to not schedule already finished jobs
+        current_job = random.randint(0, J-1)
 
-finised = False
-while not(finised):
-    job = random.randint(0, J-1)
-    operation = current_operation[job]
-    machine = Jobs[job][current_operation[job]][0]
-    req_time = Jobs[job][current_operation[job]][1]
-    MOT[machine].append((job, operation, current_time[machine], current_time[machine] + ))
+    operation = current_operation[current_job]
+    machine = Jobs[current_job][operation][0]
+    req_time = Jobs[current_job][operation][1]
 
-    print(MOT)
-    break
+    if current_time[machine] < prev_operation_end_time[current_job]: # Check for previous operation completion
+        start_time = prev_operation_end_time[current_job]
+    else:
+        start_time = current_time[machine]
+    end_time = start_time + req_time
+    prev_operation_end_time[current_job] = end_time
+    current_time[machine] = end_time
+    current_operation[current_job] += 1
+    if current_operation[current_job] == M: # Check for marking a job as finished
+        finished_jobs += 1
+    print("Current Time", current_time)
+    print("Current Operation", current_operation)
+    MOT[machine].append((current_job, operation, start_time, end_time))
+    print("Chromosome", MOT)
+    iteration += 1
+
+#     print(MOT)
+#     break
